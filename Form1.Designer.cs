e80771abd1e3f429acd3bd19b0f8a4861f3f8d24
Abood <abdallhsamy99@gmail.com>
2024-12-20 17:05:34 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Simple_Crud_App
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlSideMenu = new System.Windows.Forms.Panel();
+            this.btnEmployees = new System.Windows.Forms.Button();
+            this.btnLoans = new System.Windows.Forms.Button();
+            this.btnBooks = new System.Windows.Forms.Button();
+            this.btnMangeMembers = new System.Windows.Forms.Button();
+            this.pnl_BlockSideMenu = new System.Windows.Forms.Panel();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.pnl_BlockMain = new System.Windows.Forms.Panel();
+            this.guna2ControlBox3 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2ControlBox2 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dgvLibrary = new System.Windows.Forms.DataGridView();
+            this.pnlSideMenu.SuspendLayout();
+            this.pnlMain.SuspendLayout();
+            this.pnl_BlockMain.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLibrary)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlSideMenu
+            // 
+            this.pnlSideMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(21)))), ((int)(((byte)(24)))));
+            this.pnlSideMenu.Controls.Add(this.btnEmployees);
+            this.pnlSideMenu.Controls.Add(this.btnLoans);
+            this.pnlSideMenu.Controls.Add(this.btnBooks);
+            this.pnlSideMenu.Controls.Add(this.btnMangeMembers);
+            this.pnlSideMenu.Controls.Add(this.pnl_BlockSideMenu);
+            this.pnlSideMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlSideMenu.Location = new System.Drawing.Point(0, 0);
+            this.pnlSideMenu.Name = "pnlSideMenu";
+            this.pnlSideMenu.Size = new System.Drawing.Size(210, 788);
+            this.pnlSideMenu.TabIndex = 1;
+            this.pnlSideMenu.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlSideMenu_Paint);
+            // 
+            // btnEmployees
+            // 
+            this.btnEmployees.BackColor = System.Drawing.Color.Red;
+            this.btnEmployees.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnEmployees.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEmployees.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmployees.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnEmployees.Location = new System.Drawing.Point(0, 386);
+            this.btnEmployees.Name = "btnEmployees";
+            this.btnEmployees.Size = new System.Drawing.Size(210, 99);
+            this.btnEmployees.TabIndex = 5;
+            this.btnEmployees.Text = "Employees";
+            this.btnEmployees.UseVisualStyleBackColor = false;
+            this.btnEmployees.Click += new System.EventHandler(this.btnEmployees_Click);
+            // 
+            // btnLoans
+            // 
+            this.btnLoans.BackColor = System.Drawing.Color.Red;
+            this.btnLoans.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLoans.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoans.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoans.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnLoans.Location = new System.Drawing.Point(0, 287);
+            this.btnLoans.Name = "btnLoans";
+            this.btnLoans.Size = new System.Drawing.Size(210, 99);
+            this.btnLoans.TabIndex = 4;
+            this.btnLoans.Text = "Loans";
+            this.btnLoans.UseVisualStyleBackColor = false;
+            this.btnLoans.Click += new System.EventHandler(this.btnLoans_Click);
+            // 
+            // btnBooks
+            // 
+            this.btnBooks.BackColor = System.Drawing.Color.Red;
+            this.btnBooks.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnBooks.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBooks.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBooks.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnBooks.Location = new System.Drawing.Point(0, 188);
+            this.btnBooks.Name = "btnBooks";
+            this.btnBooks.Size = new System.Drawing.Size(210, 99);
+            this.btnBooks.TabIndex = 3;
+            this.btnBooks.Text = "Books";
+            this.btnBooks.UseVisualStyleBackColor = false;
+            this.btnBooks.Click += new System.EventHandler(this.btnBooks_Click);
+            // 
+            // btnMangeMembers
+            // 
+            this.btnMangeMembers.BackColor = System.Drawing.Color.Red;
+            this.btnMangeMembers.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMangeMembers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMangeMembers.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMangeMembers.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnMangeMembers.Location = new System.Drawing.Point(0, 89);
+            this.btnMangeMembers.Name = "btnMangeMembers";
+            this.btnMangeMembers.Size = new System.Drawing.Size(210, 99);
+            this.btnMangeMembers.TabIndex = 2;
+            this.btnMangeMembers.Text = "Members";
+            this.btnMangeMembers.UseVisualStyleBackColor = false;
+            this.btnMangeMembers.Click += new System.EventHandler(this.btnMangeMembers_Click);
+            // 
+            // pnl_BlockSideMenu
+            // 
+            this.pnl_BlockSideMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_BlockSideMenu.Location = new System.Drawing.Point(0, 0);
+            this.pnl_BlockSideMenu.Name = "pnl_BlockSideMenu";
+            this.pnl_BlockSideMenu.Size = new System.Drawing.Size(210, 89);
+            this.pnl_BlockSideMenu.TabIndex = 0;
+            this.pnl_BlockSideMenu.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnl_BlockSideMenu_MouseDown);
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.BackColor = System.Drawing.Color.Blue;
+            this.pnlMain.Controls.Add(this.dgvLibrary);
+            this.pnlMain.Controls.Add(this.pnl_BlockMain);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(210, 0);
+            this.pnlMain.MinimumSize = new System.Drawing.Size(400, 400);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(836, 788);
+            this.pnlMain.TabIndex = 2;
+            // 
+            // pnl_BlockMain
+            // 
+            this.pnl_BlockMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(21)))), ((int)(((byte)(24)))));
+            this.pnl_BlockMain.Controls.Add(this.guna2ControlBox3);
+            this.pnl_BlockMain.Controls.Add(this.guna2ControlBox2);
+            this.pnl_BlockMain.Controls.Add(this.guna2ControlBox1);
+            this.pnl_BlockMain.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_BlockMain.Location = new System.Drawing.Point(0, 0);
+            this.pnl_BlockMain.Name = "pnl_BlockMain";
+            this.pnl_BlockMain.Size = new System.Drawing.Size(836, 89);
+            this.pnl_BlockMain.TabIndex = 0;
+            this.pnl_BlockMain.Paint += new System.Windows.Forms.PaintEventHandler(this.pnl_BlockMain_Paint);
+            this.pnl_BlockMain.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnl_BlockMain_MouseDown);
+            // 
+            // guna2ControlBox3
+            // 
+            this.guna2ControlBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox3.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.guna2ControlBox3.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(152)))), ((int)(((byte)(166)))));
+            this.guna2ControlBox3.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox3.Location = new System.Drawing.Point(702, 0);
+            this.guna2ControlBox3.Name = "guna2ControlBox3";
+            this.guna2ControlBox3.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox3.TabIndex = 2;
+            this.guna2ControlBox3.TabStop = false;
+            // 
+            // guna2ControlBox2
+            // 
+            this.guna2ControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox2.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MaximizeBox;
+            this.guna2ControlBox2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(152)))), ((int)(((byte)(166)))));
+            this.guna2ControlBox2.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox2.Location = new System.Drawing.Point(746, 0);
+            this.guna2ControlBox2.Name = "guna2ControlBox2";
+            this.guna2ControlBox2.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox2.TabIndex = 1;
+            this.guna2ControlBox2.TabStop = false;
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(152)))), ((int)(((byte)(166)))));
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(791, 0);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox1.TabIndex = 0;
+            this.guna2ControlBox1.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.updateToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(113, 70);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.editToolStripMenuItem.Text = "Edit ";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            // 
+            // updateToolStripMenuItem
+            // 
+            this.updateToolStripMenuItem.Name = "updateToolStripMenuItem";
+            this.updateToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.updateToolStripMenuItem.Text = "Update";
+            // 
+            // dgvLibrary
+            // 
+            this.dgvLibrary.BackgroundColor = System.Drawing.Color.Indigo;
+            this.dgvLibrary.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLibrary.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvLibrary.Location = new System.Drawing.Point(0, 89);
+            this.dgvLibrary.Name = "dgvLibrary";
+            this.dgvLibrary.Size = new System.Drawing.Size(836, 699);
+            this.dgvLibrary.TabIndex = 4;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1046, 788);
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.pnlSideMenu);
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.FrmMain_Resize);
+            this.pnlSideMenu.ResumeLayout(false);
+            this.pnlMain.ResumeLayout(false);
+            this.pnl_BlockMain.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLibrary)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlSideMenu;
+        private System.Windows.Forms.Panel pnl_BlockSideMenu;
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Panel pnl_BlockMain;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox3;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox2;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private System.Windows.Forms.Button btnMangeMembers;
+        private System.Windows.Forms.Button btnEmployees;
+        private System.Windows.Forms.Button btnLoans;
+        private System.Windows.Forms.Button btnBooks;
+        private System.Windows.Forms.DataGridView dgvLibrary;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business_L;
+
+namespace Simple_Crud_App
+{
+    public partial class FrmMain : baseForm
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+            this.Padding = new Padding(2);
+            this.MinimumSize = new Size(700, 600);
+
+            this.Size = new Size(700, 700);
+
+            ConfigureDataGridView();
+
+
+
+        }
+
+        private void ConfigureDataGridView()
+        {
+            dgvLibrary.ContextMenuStrip = contextMenuStrip1;
+            // Fill the container
+            dgvLibrary.Dock = DockStyle.Fill;
+
+            // Allow column widths to adjust
+            dgvLibrary.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            // Allow row heights to adjust
+            dgvLibrary.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
+            // Enable word wrapping for cell content
+            dgvLibrary.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+
+            // Refresh row sizes after configuration
+            dgvLibrary.AutoResizeRows(DataGridViewAutoSizeRowsMode.AllCells);
+        }
+
+        private void AdjustSides()
+        {
+            switch (this.WindowState)
+            {
+                case FormWindowState.Maximized: //Maximized form (After)
+                    this.Padding = new Padding(8, 8, 8, 0); break;
+               default: //Restored form (After)
+                    if (this.Padding.Top != 2)
+                        this.Padding = new Padding(2);
+                    break;
+            }
+        }
+ 
+
+        private void AdjustPnls()
+        {
+            pnlSideMenu.Width = (int)(0.19 * ClientSize.Width);
+            pnl_BlockMain.Height= (int)(0.14 * ClientSize.Height);
+
+        }
+
+        private void FrmMain_Resize(object sender, EventArgs e)
+        {
+            AdjustSides();
+            AdjustPnls(); 
+            
+        }
+        private void adjustButtons()
+        {
+            foreach(Control control in pnlSideMenu.Controls) {
+                control.Height = Convert.ToInt32(pnlSideMenu.Height * .18);
+            }
+        }
+
+        private void pnl_BlockMain_MouseDown(object sender, MouseEventArgs e)
+        {
+            Draggable();
+        }
+
+        private void pnl_BlockSideMenu_MouseDown(object sender, MouseEventArgs e)
+        {
+            Draggable();
+        }
+
+        private void btnMangeMembers_Click(object sender, EventArgs e)
+        {
+            dgvLibrary.DataSource=clsMember.ListAllMembers();
+            dgvLibrary.Tag = "Mem";
+        }
+
+        private void btnBooks_Click(object sender, EventArgs e)
+        {
+            dgvLibrary.DataSource = clsBook.ListAllBooks();
+            dgvLibrary.Tag = "book";
+
+        }
+
+        private void btnLoans_Click(object sender, EventArgs e)
+        {
+            dgvLibrary.DataSource = clsLoan.ListAllLoans();
+            dgvLibrary.Tag = "loan";
+
+        }
+
+        private void btnEmployees_Click(object sender, EventArgs e)
+        {
+            dgvLibrary.DataSource = clsEmployee.ListAllEmployees();
+            dgvLibrary.Tag = "emp";
+        }
+
+        private void pnl_BlockMain_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pnlSideMenu_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
